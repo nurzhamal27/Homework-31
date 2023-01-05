8ee6f56d518c1368c68869db3f55d758acf1fe4a
Ismail kyzy Nurzhamal <n.ismailkyzy@gmail.com>
2023-01-05 13:12:15 +0600

    change methods

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
-package kg.attractor.java;
-
  import util.RestaurantOrders;
 
  import static java.util.stream.Collectors.*;
  import static java.util.Comparator.*;
-
-// используя статические imports
-// мы импортируем *всё* из Collectors и Comparator.
-// теперь нам доступны такие операции как
-// toList(), toSet() и прочие, без указания уточняющего слова Collectors. или Comparator.
-// вот так было до импорта Collectors.toList(), теперь стало просто toList()
-
 
 public class Main {
 
@@ -20,7 +11,7 @@ public class Main {
         // выберите любое количество заказов, какое вам нравится.
 
         var orders = RestaurantOrders.read("orders_100.json").getOrders();
-        //var orders = RestaurantOrders.read("orders_1000.json").getOrders();
+//        var orders = RestaurantOrders.read("orders_1000.json").getOrders();
         //var orders = RestaurantOrders.read("orders_10_000.json").getOrders();
 
 
